13b03e38a3478c52addda0dfd547b0e93f652459
cyt <470922684@qq.com>
2019-08-07 17:48:32 +0800

    first commit

---
+++ 子组件.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>子组件的值：{{sontype}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{}
+  },
+  props: ['sontype']
+}
+</script>
+
+<style>
+
+</style>
